7be7269b0f6c4294700dd51f72f5a0fdc329bf5c
Ladyjhen <145974043+Ladyjhen@users.noreply.github.com>
2024-04-05 13:53:15 +0800

    commit

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+
+
+const photosMarkup = createGalleryItem(galleryItems)
+
+function createGalleryItem(array) {
+    return array.map(({preview, original,description})=>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        
+        </a> </li>`;
+    }).join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend",photosMarkup);
+const galleryHandler=new SimpleLightbox(".gallery a", {captionsData: "alt", captionDelay:150});
+galleryHandler.on(show.simplelightbox);
